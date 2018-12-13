40de51fad9c98e85c95fc9b7919703eb83703a2b
nereid <nas.kuleshova@gmail.com>
2018-12-13 17:27:32 +0300

    end index

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -66,9 +66,9 @@ $(document).on('click', '.gif img', function (e) {
 
 if ($('.js-detail').length > 0) {
   var container = $('.container');
-  var width = container.css('width');
+  var width = parseInt(container.css('width')) - parseInt(container.css('paddingLeft')) - parseInt(container.css('paddingRight'));
   var offsetNormal = container.offset().left + parseInt(container.css('paddingLeft'));
-  $('.js-detail').css('width', width);
+  $('.js-detail').css('width', width + 'px');
   $(document).on('click', '.js-btn-detail', function (e) {
     e.preventDefault();
     var item = $(this).parents('.js-detail-wrap').find('.js-detail');
@@ -76,9 +76,45 @@ if ($('.js-detail').length > 0) {
     if (!item.hasClass('done')) {
       item.slideToggle({
         start: function start() {
-          var marginLeft = ($(this).offset().left - offsetNormal) * -1 + 'px';
+          var thisOffset = $(this).offset().left - offsetNormal;
+          var marginLeft = thisOffset * -1 + 'px';
+          var parentWidth = parseInt($(this).parents('.js-detail-wrap').css('width'));
           $(this).css('marginLeft', marginLeft);
           $(this).addClass('done');
+          $('.single-card__detail-tr', this).css('left', thisOffset + parentWidth / 2 - 12 + 'px');
+          if ($('.single-card__slider', this).length > 0) {
+            $('.single-card__slider', this).slick({
+              slidesToShow: 5,
+              nextArrow: '<button class="single-card__next">\n' + '            <svg>\n' + '              <use xlink:href="assets/images/svg.svg#arrow">\n' + '              </use>\n' + '            </svg>\n' + '          </button>',
+              prevArrow: '<button class="single-card__prev">\n' + '            <svg>\n' + '              <use xlink:href="assets/images/svg.svg#arrow">\n' + '              </use>\n' + '            </svg>\n' + '          </button>',
+              responsive: [{
+                breakpoint: 1200,
+                settings: {
+                  slidesToShow: 4
+                }
+              }, {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 5
+                }
+              }, {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 4
+                }
+              }, {
+                breakpoint: 576,
+                settings: {
+                  slidesToShow: 3
+                }
+              }, {
+                breakpoint: 420,
+                settings: {
+                  slidesToShow: 2
+                }
+              }]
+            });
+          }
         }
       });
     } else {
--- src/components/single-card/single-card.js
+++ src/components/single-card/single-card.js
@@ -1,8 +1,8 @@
 if ($('.js-detail').length > 0) {
   let container = $('.container');
-  let width = container.css('width');
+  let width = parseInt(container.css('width')) - parseInt(container.css('paddingLeft')) - parseInt(container.css('paddingRight'));
   let offsetNormal = container.offset().left + parseInt(container.css('paddingLeft'));
-  $('.js-detail').css('width', width);
+  $('.js-detail').css('width', width + 'px');
   $(document).on('click', '.js-btn-detail', function(e) {
     e.preventDefault();
     let item = $(this).parents('.js-detail-wrap').find('.js-detail');
@@ -10,9 +10,61 @@ if ($('.js-detail').length > 0) {
     if (!item.hasClass('done')) {
       item.slideToggle({
         start: function() {
-          let marginLeft = ($(this).offset().left - offsetNormal) * -1 + 'px';
+          let thisOffset = $(this).offset().left - offsetNormal;
+          let marginLeft = thisOffset * -1 + 'px';
+          let parentWidth = parseInt($(this).parents('.js-detail-wrap').css('width'));
           $(this).css('marginLeft', marginLeft);
           $(this).addClass('done');
+          $('.single-card__detail-tr', this).css('left', thisOffset + parentWidth / 2 - 12 + 'px');
+          if ($('.single-card__slider', this).length > 0) {
+            $('.single-card__slider', this).slick({
+              slidesToShow: 5,
+              nextArrow: '<button class="single-card__next">\n' +
+                '            <svg>\n' +
+                '              <use xlink:href="assets/images/svg.svg#arrow">\n' +
+                '              </use>\n' +
+                '            </svg>\n' +
+                '          </button>',
+              prevArrow: '<button class="single-card__prev">\n' +
+                '            <svg>\n' +
+                '              <use xlink:href="assets/images/svg.svg#arrow">\n' +
+                '              </use>\n' +
+                '            </svg>\n' +
+                '          </button>',
+              responsive: [
+                {
+                  breakpoint: 1200,
+                  settings: {
+                    slidesToShow: 4
+                  }
+                },
+                {
+                  breakpoint: 992,
+                  settings: {
+                    slidesToShow: 5
+                  }
+                },
+                {
+                  breakpoint: 768,
+                  settings: {
+                    slidesToShow: 4
+                  }
+                },
+                {
+                  breakpoint: 576,
+                  settings: {
+                    slidesToShow: 3
+                  }
+                },
+                {
+                  breakpoint: 420,
+                  settings: {
+                    slidesToShow: 2
+                  }
+                }
+              ]
+            });
+          }
         }
       });
     }
